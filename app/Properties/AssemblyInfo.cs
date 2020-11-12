0af22ad12242859c4947ccd336acd810b62f46c9
Jô Developer <joalves3carvalho@gmail.com>
2020-11-12 09:41:29 -0300

    first commit

---
+++ app/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("SystemGynControl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SystemGynControl")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("624a7289-3f7e-4196-a9b1-1c18645624fc")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ db/table.sql
@@ -0,0 +1,39 @@
+﻿CREATE TABLE [dbo].[student]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [name] VARCHAR(100) NULL, 
+    [cpf] VARCHAR(14) NOT NULL, 
+    [cep] VARCHAR(10) NULL, 
+    [district] VARCHAR(100) NULL, 
+    [address] VARCHAR(100) NULL, 
+    [number] INT NULL, 
+    [birthday] VARCHAR(10) NULL, 
+    [city] VARCHAR(50) NULL, 
+    [state] VARCHAR(50) NULL
+)
+
+CREATE TABLE [dbo].[personal]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(100) NULL, 
+    [cpf] VARCHAR(14) NULL, 
+    [cep] VARCHAR(10) NULL, 
+    [district] VARCHAR(100) NULL, 
+    [address] VARCHAR(100) NULL, 
+    [number] INT NULL, 
+    [birthday] VARCHAR(10) NULL, 
+    [city] VARCHAR(50) NULL, 
+    [state] VARCHAR(50) NULL
+)
+
+CREATE TABLE [dbo].[responsible]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [name] VARCHAR(100) NULL, 
+    [cpf] VARCHAR(14) NOT NULL, 
+    [kinship] VARCHAR(100) NULL,
+    [student_id] INT NOT NULL,
+    FOREIGN KEY([student_id]) REFERENCES [dbo].[student]([id]) ON DELETE CASCADE
+)
+
+drop table teacher
