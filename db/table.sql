fcb5f4e24b25d3e3ac0e3a700782d3273ff12174
Jô Developer <joalves3carvalho@gmail.com>
2020-11-12 15:14:23 -0300

    modelando os dados

--- db/table.sql
+++ db/table.sql
@@ -7,10 +7,31 @@
     [district] VARCHAR(100) NULL, 
     [address] VARCHAR(100) NULL, 
     [number] INT NULL, 
-    [birthday] VARCHAR(10) NULL, 
+    [birth] VARCHAR(10) NULL, 
     [city] VARCHAR(50) NULL, 
-    [state] VARCHAR(50) NULL
+    [state] VARCHAR(50) NULL,
+    [photo] IMAGE NULL
+    
 )
+
+CREATE TABLE [dbo].[situation_student] (
+    [id]         INT          NOT NULL,
+    [situation]  VARCHAR (10) NOT NULL,
+    [student_id] INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC),
+    FOREIGN KEY ([student_id]) REFERENCES [dbo].[student] ([id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [dbo].[responsible_student]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [name] VARCHAR(100) NULL, 
+    [cpf] VARCHAR(14) NOT NULL, 
+    [kinship] VARCHAR(100) NULL,
+    [student_id] INT NOT NULL,
+    FOREIGN KEY([student_id]) REFERENCES [dbo].[student]([id]) ON DELETE CASCADE
+)
+
 
 CREATE TABLE [dbo].[personal]
 (
@@ -21,19 +42,31 @@ CREATE TABLE [dbo].[personal]
     [district] VARCHAR(100) NULL, 
     [address] VARCHAR(100) NULL, 
     [number] INT NULL, 
-    [birthday] VARCHAR(10) NULL, 
+    [birth] VARCHAR(10) NULL, 
     [city] VARCHAR(50) NULL, 
     [state] VARCHAR(50) NULL
 )
 
-CREATE TABLE [dbo].[responsible]
+CREATE TABLE [dbo].[package]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
-    [name] VARCHAR(100) NULL, 
-    [cpf] VARCHAR(14) NOT NULL, 
-    [kinship] VARCHAR(100) NULL,
-    [student_id] INT NOT NULL,
-    FOREIGN KEY([student_id]) REFERENCES [dbo].[student]([id]) ON DELETE CASCADE
+	[id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [description] VARCHAR(100) NULL, 
+    [value] DECIMAL(18, 2) NULL
 )
 
-drop table teacher
+CREATE TABLE [dbo].[situation_package]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [situation] VARCHAR(10) NOT NULL,
+	package_id INT NOT NULL,
+	FOREIGN KEY ([package_id]) REFERENCES [dbo].[package]([id]) ON DELETE CASCADE
+)
+
+CREATE TABLE [dbo].[billing_parameters]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [value_penalty] DECIMAL(18, 2) NULL, 
+    [value_interest] DECIMAL(18, 2) NULL, 
+    [package_id] INT NULL,
+	FOREIGN KEY ([package_id]) REFERENCES [dbo].[package]([id]) ON DELETE CASCADE
+)
